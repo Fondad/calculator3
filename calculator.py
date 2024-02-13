8d53b1642f80e1a0e40490cadd27cdd7d9482145
Vsevolod Demidov <fondad@ukr.net>
2024-02-13 09:11:29 +0200

    Multiple operation was included into calculator

--- calculator.py
+++ calculator.py
@@ -2,4 +2,8 @@ def calculator(x,y,sign):
     # This is an unwanted but staged comment 
     if sign == "+":
         return x + y
+    elif sign == "-":
+        return x - y
+    elif sign == "*":
+        return x * y
     return None
